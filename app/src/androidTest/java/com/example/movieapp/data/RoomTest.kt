a50182d4e08b7647a196e16376b092700697ec58
Ahmed Elesdody <37310898+elesdody5@users.noreply.github.com>
2020-06-04 23:50:30 +0200

    init commit create room local data source

---
+++ app/src/androidTest/java/com/example/movieapp/data/RoomTest.kt
@@ -0,0 +1,48 @@
+package com.example.movieapp.data
+
+import androidx.room.Room
+import androidx.room.paging.LimitOffsetDataSource
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.movieapp.data.local_data.MovieDao
+import com.example.movieapp.data.local_data.MovieDatabase
+import com.example.movieapp.data.local_data.entity.LocalMovie
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class RoomTest {
+    private lateinit var movieDao: MovieDao
+    private lateinit var db: MovieDatabase
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, MovieDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+        movieDao = db.movieDao
+    }
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetList() {
+        val movieList = listOf(LocalMovie(id = 1), LocalMovie(id = 2))
+
+        movieDao.insert(movieList)
+        val factory = movieDao.getMovies()
+        val list = (factory.create() as LimitOffsetDataSource).loadRange(0, 2)
+        Assert.assertEquals(list[0].id, 1)
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/data/local_data/MovieDao.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.data.local_data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.paging.DataSource
+import com.example.movieapp.data.local_data.entity.LocalMovie
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(movies: List<LocalMovie>)
+
+
+    @Query("SELECT * FROM LocalMovie")
+    fun getMovies(): DataSource.Factory<Int, LocalMovie>
+}
