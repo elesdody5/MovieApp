2fb37bf2b72795de4619a08764e1f29221072038
Ahmed Elesdody <ahmed.alasudi@vodafone.com>
2021-09-16 13:58:01 +0200

    show download progress

--- app/src/main/java/com/example/movieapp/ui/download/DownloadFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/download/DownloadFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.movieapp.MovieApplication
@@ -22,10 +24,10 @@ class DownloadFragment : Fragment() {
 
     lateinit var binding: FragmentDownloadBinding
 
-//    @Inject
-//    lateinit var viewModelFactory: ViewModelProvider.Factory
-//
-//    private val viewModel by viewModels<DownloadViewModel> { viewModelFactory }
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val viewModel by viewModels<DownloadViewModel> { viewModelFactory }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -41,10 +43,19 @@ class DownloadFragment : Fragment() {
         binding = FragmentDownloadBinding.bind(view)
 
         args.value.movie.apply {
-//            binding.name.text = name
-//            viewModel.downloadFile(this)
+            binding.name.text = name
+            viewModel.downloadFile(this)
         }
-
+        viewModel.networkError.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(context, "\uD83D\uDE28 Wooops $it", Toast.LENGTH_LONG).show()
+        })
+        viewModel.progress.observe(viewLifecycleOwner, Observer {
+            binding.progress.progress = it
+        })
+        viewModel.complete.observe(viewLifecycleOwner, Observer {
+            binding.checked.visibility = View.VISIBLE
+            Toast.makeText(context, "\uD83D\uDE0A Download complete", Toast.LENGTH_LONG).show()
+        })
         return view
     }
 
--- app/src/main/java/com/example/movieapp/ui/download/DownloadViewModel.kt
+++ app/src/main/java/com/example/movieapp/ui/download/DownloadViewModel.kt
@@ -3,6 +3,8 @@ package com.example.movieapp.ui.download
 import android.app.Application
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.movieapp.data.entity.Movie
 import com.example.movieapp.data.repository.Repository
@@ -13,13 +15,30 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import okhttp3.ResponseBody
 import retrofit2.Response
+import java.io.BufferedInputStream
 import java.io.File
+import java.io.InputStream
 import javax.inject.Inject
+
 
 class DownloadViewModel @Inject constructor(
     private val repository: Repository,
     private val application: Application
 ) : ViewModel() {
+
+    private val _progress = MutableLiveData<Int>()
+
+    val progress: LiveData<Int>
+        get() = _progress
+
+    private val _complete = MutableLiveData<Boolean>()
+
+    val complete: LiveData<Boolean>
+        get() = _complete
+
+    private val _networkError = MutableLiveData<String>()
+    val networkError: LiveData<String>
+        get() = _networkError
 
     fun downloadFile(movie: Movie) {
         movie.url?.let { it ->
@@ -27,19 +46,9 @@ class DownloadViewModel @Inject constructor(
                 .flatMap(object : Function1<Response<ResponseBody>, Observable<File>> {
                     override fun invoke(responseBodyResponse: Response<ResponseBody>): Observable<File> {
                         return Observable.create { emitter ->
-                            val file = File(movie.name ?: "")
-
-                            val fileOutputStream =
-                                application.openFileOutput(file.name, Context.MODE_PRIVATE)
-
-
-                            fileOutputStream.write(
-                                responseBodyResponse.body()?.source().toString().toByteArray()
-                            )
-                            fileOutputStream.close()
+                            val file = writeFile(responseBodyResponse.body(), movie.name)
                             emitter.onNext(file)
                             emitter.onComplete()
-
                         }
                     }
                 })
@@ -48,11 +57,13 @@ class DownloadViewModel @Inject constructor(
                 .subscribe(object : Observer<File?> {
                     override fun onComplete() {
                         Log.d("downloadZipFile", "onCompleted")
+                        _complete.value = true
                     }
 
                     override fun onError(e: Throwable) {
                         e.printStackTrace()
                         Log.d("downloadZipFile", "Error " + e.message)
+                        _networkError.value = e.message
                     }
 
                     override fun onNext(file: File) {
@@ -64,5 +75,34 @@ class DownloadViewModel @Inject constructor(
                     }
                 })
         }
+    }
+
+    private fun writeFile(body: ResponseBody?, movieName: String?): File {
+        val fileSize = body?.contentLength()
+
+        val bis: InputStream = BufferedInputStream(
+            body?.byteStream()
+        )
+        val data = ByteArray(1024 * 4)
+        var count: Int = 0
+        var total: Long = 0
+
+        val file = File(movieName)
+
+        val fileOutputStream =
+            application.openFileOutput(file.name, Context.MODE_PRIVATE)
+
+        while (bis.read(data).also { count = it } !== -1) {
+            total += count
+            val progress = (total * 100 / fileSize!!).toInt()
+            _progress.value = progress
+            Log.i("download view model", "progress$progress")
+            fileOutputStream.write(data, 0, count)
+        }
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        bis.close()
+
+        return file
     }
 }
